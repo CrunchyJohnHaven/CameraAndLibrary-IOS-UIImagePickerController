32f7b701b2bb5bc2bfe14411c2f279efac464109
John Bradley <johnhavenbradley@gmail.com>
2018-03-18 20:16:52 -0700

    update

--- CameraLibrary/CameraLibrary/ViewController.swift
+++ CameraLibrary/CameraLibrary/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imagePicked: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,25 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func openCamerButton(_ sender: Any) {
+        //check ig the camera is available on the device
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+//            declare a variable of image picker controller
+            var imagePicker = UIImagePickerController()
+//            set its delegate
+            imagePicker.delegate = self
+//            set its source type
+            imagePicker.sourceType = .camera;
+//            tell camera not to go into edit mode
+            imagePicker.allowsEditing = false
+//            present the camera controller on the bottom of the screen
+            self.presentViewController(imagePicker, animated: true, completion: nil)
+        }
+    }
+    @IBAction func openPhotoLibraryButton(_ sender: Any) {
+        
+    }
+    
 
 }
 
